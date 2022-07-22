323ad17a479b448cd0390136d5b48ff1e420b145
Andry Hernández <andryj9622@gmail.com>
2022-07-21 22:48:28 -0400

    F#~:added drone and medication serializer

---
+++ apps/core/serializers.py
@@ -0,0 +1,37 @@
+import re
+import string
+from rest_framework import serializers
+from apps.core.models import Drone, Medication
+
+
+class DroneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drone
+        field = ['uuid', 'serial_no', 'model', 'weight_limit',
+                 'battery', 'state', 'created_at', 'updated_at']
+
+
+class MedicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medication
+        field = ['uuid', 'name', 'weight', 'code',
+                 'image', 'created_at', 'updated_at']
+
+    def validate_code(self, name):
+        allowed = string.ascii_letters+string.digits+'_'+'-'
+
+        if not re.match(r'^[%s]+$' % allowed, name):
+            raise serializers.ValidationError(
+                'Name must be alphanumeric, contain only letters, digits, underscore and hyphen.')
+
+        return name
+
+    def validate_code(self, code):
+        allowed = string.ascii_uppercase + string.digits + '_'
+
+        if not re.match(r'^[%s]+$' % allowed, code):
+            raise serializers.ValidationError(
+                'Code must be contain only upper case letters, underscore and numbers.'
+            )
+
+        return code
