de605d2bc2db6fee296340f3700e45fc0c471069
Andry Hernández <andryj9622@gmail.com>
2022-07-21 23:30:56 -0400

    F#~:detail drone and availables

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,7 +3,9 @@ from rest_framework import routers
 from apps.core import views
 router = routers.DefaultRouter()
 
-router.register(r'drones', views.Drones)
+router.register(r'drones/list', views.Drones)
+router.register(r'drones/detail', views.DroneDetail)
+router.register(r'drones/available', views.AvailableDrones)
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,5 +3,28 @@ from apps.core import models, serializers
 
 
 class Drones(generics.ListCreateAPIView, viewsets.GenericViewSet):
+    """
+    List all drones, or create a new drone.
+    """
     queryset = models.Drone.objects.all()
+    serializer_class = serializers.DroneSerializer
+
+
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView, viewsets.GenericViewSet):
+    """
+    Retrieve, update or delete a drone instance.
+    """
+    queryset = models.Drone.objects.all()
+    serializer_class = serializers.DroneSerializer
+    lookup_field = 'uuid'
+
+
+class AvailableDrones(generics.ListAPIView, viewsets.GenericViewSet):
+    """
+    List all available drones (considere as available the IDLE stete and battery status up or equal to 25%).
+    """
+    queryset = models.Drone.objects.filter(
+        state=models.DRONE_STATUS.IDLE,
+        battery__gte=25
+    )
     serializer_class = serializers.DroneSerializer
